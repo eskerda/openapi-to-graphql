704f65d7b901c48cf51beee469021b67a141c65f
eskerda <eskerda@gmail.com>
2019-09-11 16:25:20 +0200

    Make it a single package
    npm does not support paths for installing on git repos. This should also
    not go upstream.

---
+++ lib/graphql_tools.d.ts
@@ -0,0 +1,15 @@
+/**
+ * Utilities related to GraphQL.
+ */
+import {
+  GraphQLObjectType as GQObjectType,
+  GraphQLInputObjectType as GQInputObjectType
+} from "graphql";
+/**
+ * Returns empty GraphQLObjectType.
+ */
+export declare function getEmptyObjectType(name: string): GQObjectType;
+/**
+ * Returns empty GraphQLInputObjectType.
+ */
+export declare function getEmptyInputObjectType(): GQInputObjectType;
